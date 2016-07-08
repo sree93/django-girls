a794136999f0ba7cfc6372f0aaab2bb342fc3c8f
Srinath <srinath@cowhite.com>
2016-07-08 16:22:05 +0530

    Add/Edit Post implemented

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -17,3 +18,38 @@ def post_detail(request, pk):
 		'post' :	post,
 	}
 	return render(request, 'blog/post_detail.html', context)
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+
+
+	form = PostForm()
+	context = {
+		'form'	: form
+	}
+	return render(request, 'blog/post_edit.html', context)
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+
+
+	form = PostForm(instance=post)
+	context = {
+		'form'	: form
+	}
+	return render(request, 'blog/post_edit.html', context)
